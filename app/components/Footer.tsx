0d7bbd7f91b2c6114f8cb5d8e78d9e5be59a2192
rocky <rocky@localhost.localdomain>
2023-11-24 04:44:53 -0500

    header, footer, navbar

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="absolute bottom-0 w-full bg-white text-center py-[.5rem] font-extralight">
-            Martin Ludka &copy; SPSMT 2023
+            Martin Ludka &copy; SPST Martin 2023
         </div>
     );
 };
