d5b83a23ad57da6d53cec5b1ed8e8530bcec7029
TerThesz <prdacik.kakacik@rindi.spil>
2024-01-26 11:10:59 +0100

    [frontend] many: moved backend

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,23 @@
+import localFont from 'next/font/local';
+
+const font = localFont({
+  src: '../../common/fonts//creattion/Creattion.otf',
+});
+
 const Header = () => {
-    return (
-        <>
-            <header className="w-full top-0 left-0 bg-white border-b border-slate-700 border-opacity-5 text-gray-800 px-[1rem] py-[.8rem]">
-                <div className="text-[1.5rem] font-light tracking-wide text-center w-full flex-row flex justify-center items-center gap-[2rem]">
-                    <hr className="w-[8rem]" />
-                    <h1>Matej Ľudma</h1>
-                    <hr className="w-[8rem]" />
-                </div>
-            </header>
-        </>
-    );
+  return (
+    <>
+      <header className="w-full top-0 left-0 bg-white border-b border-slate-700 border-opacity-5 text-gray-800 px-[1rem] py-[.8rem]">
+        <div className="text-[1.5rem] font-light tracking-wide text-center w-full flex-row flex justify-center items-center gap-[2rem]">
+          <hr className="w-[8rem] opacity-90" />
+          <h1 className={`${font.className} text-4xl`}>
+            Matej Ludma
+          </h1>
+          <hr className="w-[8rem] opacity-90" />
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
