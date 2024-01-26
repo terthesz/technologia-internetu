de06e82090731ad7ab1256036ae21f6258ce6732
TerThesz <prdacik.kakacik@rindi.spil>
2024-01-26 11:05:26 +0100

    [frontend] many: moved backend

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,59 +6,54 @@ import { useEffect, useRef, useState } from 'react';
 import { usePageContext } from '../template';
 
 const Navbar = ({ setPage }: any) => {
-    const path = usePathname();
-    const navbar = useRef<any>();
-    const currentPage = usePageContext();
-    let items: any;
+  const path = usePathname();
+  const navbar = useRef<any>();
+  const currentPage = usePageContext();
+  let items: any;
 
-    useEffect(() => {
-        if (!path || !navbar) return;
+  useEffect(() => {
+    if (!path || !navbar) return;
 
-        if (!items)
-            items = Array.from(navbar.current!.children);
+    if (!items)
+      items = Array.from(navbar.current!.children);
 
-        const parts = path.split('/');
-        const page = parts[parts.length - 1];
+    const parts = path.split('/');
+    const page = parts[parts.length - 1];
 
-        const index = pages.findIndex(
-            (entry) => entry.page == page
-        );
-        if (!index) return;
-    }, [path, navbar]);
-
-    function handleClick(index: number) {
-        window.history.replaceState(
-            {},
-            pages[index] + '',
-            `/${pages[index].page}`
-        );
-        setPage(index);
-    }
-
-    return (
-        <nav
-            ref={navbar}
-            className="h-full left-0 flex flex-col w-[25rem] gap-[2rem] justify-center items-center border-r border-slate-700 border-opacity-5"
-        >
-            {pages.map((page, i) => (
-                <button
-                    key={i}
-                    onClick={() => handleClick(i)}
-                >
-                    <p
-                        data-role="nav-item"
-                        className={`text-lg font-light ${
-                            currentPage === i
-                                ? 'active'
-                                : ''
-                        }`}
-                    >
-                        {page.text}
-                    </p>
-                </button>
-            ))}
-        </nav>
+    const index = pages.findIndex(
+      (entry) => entry.page == page
     );
+    if (!index) return;
+  }, [path, navbar]);
+
+  function handleClick(index: number) {
+    window.history.replaceState(
+      {},
+      pages[index] + '',
+      `/${pages[index].page}`
+    );
+    setPage(index);
+  }
+
+  return (
+    <nav
+      ref={navbar}
+      className="h-full left-0 flex flex-col w-[25rem] gap-[2rem] justify-center items-center border-r border-slate-700 border-opacity-5"
+    >
+      {pages.map((page, i) => (
+        <button key={i} onClick={() => handleClick(i)}>
+          <p
+            data-role="nav-item"
+            className={`text-lg font-light ${
+              currentPage === i ? 'active' : ''
+            }`}
+          >
+            {page.text}
+          </p>
+        </button>
+      ))}
+    </nav>
+  );
 };
 
 export default Navbar;
